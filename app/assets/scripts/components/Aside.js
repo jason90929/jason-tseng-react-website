7712b7d2c2a016c69a58087a207f5d620cdd8946
Jason Tseng <tsaistorm@gmail.com>
2016-08-25 17:40:40 +0800

    Menu 小尺寸的list 切換挖洞

--- app/assets/scripts/components/Aside.js
+++ app/assets/scripts/components/Aside.js
@@ -1,15 +1,39 @@
 import React from 'react';
 import MenuLink from '../containers/MenuLink';
 
-const Aside = ({content, onClick}) => {
-    var headerClassName = '';
+const Aside = ({content}) => {
 
     if (content === 'CONTENT') {
-        headerClassName = ' has-content fade-in';
+        return (
+            <aside className="menu has-content fade-in">
+                <a href="javascript:;" className="toggle">
+                    <i className="fa fa-list"/>
+                </a>
+                <nav className="main-menu">
+                    <ul className="bask">
+                        <MenuLink filter="HOME">
+                            Home
+                        </MenuLink>
+                        <MenuLink filter="ABOUT">
+                            About
+                        </MenuLink>
+                        <MenuLink filter="SKILLS">
+                            Skills
+                        </MenuLink>
+                        <MenuLink filter="PORTFOLIO">
+                            Portfolio
+                        </MenuLink>
+                        <MenuLink filter="CONTACT">
+                            Contact
+                        </MenuLink>
+                    </ul>
+                </nav>
+            </aside>
+        );
     }
 
     return (
-        <aside className={'menu' + headerClassName}>
+        <aside className="menu">
             <nav className="main-menu">
                 <ul className="bask">
                     <MenuLink filter="HOME">
