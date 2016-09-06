770084b7f5675d8e0d26aa697a13e25045fd49c6
Jason Tseng <tsaistorm@gmail.com>
2016-09-06 11:15:50 +0800

    修正 Menu status 存取錯誤的問題

--- app/assets/scripts/containers/MenuLink.js
+++ app/assets/scripts/containers/MenuLink.js
@@ -4,7 +4,7 @@ import Menu from '../components/Menu';
 
 const mapStateToProps = (state, ownProps) => {
     return {
-        active: ownProps.filter === state.language
+        active: ownProps.filter === state.content
     };
 };
 
